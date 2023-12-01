ecb1414fd603bcb4e924db5d9b4c3a5f03d5ab9a
Khun <khunsuekthai_bua@cmu.ac.th>
2023-12-01 15:35:33 +0700

    Labed 1

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,35 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+
+    vector<int> num;
+
+    int input;
+
+    while(true){
+        cout << "Enter an integer: ";
+        cin >> input;
+        num.push_back(input);
+        if(input == 0){
+            break;
+        }
+    }
+
+    int i = 0 , sumE = 0 , sumO = 0;
+    
+    while(i < num.size()){
+        if(num[i]%2 == 0){
+            sumE += 1;
+        } else{
+            sumO += 1;
+        }
+        i++;
+    }
+
+    cout << "#Even numbers = " << sumE-1 << endl;
+    cout << "#Odd numbers = " << sumO << endl;
     return 0;
+
 }
